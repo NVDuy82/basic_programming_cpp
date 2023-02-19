7489c873498b41b2ff2aecd59cf537b8a94773ce
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-02-20 01:52:29 +0700

    Update big_modulo.cpp

--- big_modulo.cpp
+++ big_modulo.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
+
 #include <cmath>
- 
+
 using namespace std;
 
-long long modulo(int n, int k, int d)
-{
+long long modulo(int n, int k, int d) {
   long long r, res;
-  
-	res = 1;
-	r = n % d;
- 
-	// Sơ đồ nhân Ai Cập
-	while (k > 0)
-	{
-		if(k & 1)
-			res = (res * r) % d;
 
-		r = (r * r) % d;
-		k >>= 1;
-	}
+  res = 1;
+  r = n % d;
+
+  // Sơ đồ nhân Ai Cập
+  while (k > 0) {
+    if (k & 1) {
+      res = (res * r) % d;
+    }
+
+    r = (r * r) % d;
+    k >>= 1;
+  }
 
   return res;
 }
 
-int main()
-{
+int main() {
   // Test
   // 11^59 % 100
   cout << modulo(11, 59, 100) << "\n";
